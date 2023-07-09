b25891d6afb304da21d80b9c61acdefed710c88e
MarialiceH <marialice.hilt@gmail.com>
2023-07-09 09:44:35 -0400

    Inital commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var clear = document.querySelector("button");
+
+//console.log(clear);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "1") {
+    box1.innerText = "🐩";
+  } else if (e.key === "2") {
+    box2.innerText = "🍔";
+  } else if (e.key === "3") {
+    box3.innerText = "💕";
+  }
+});
+
+clear.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
